1268be340c6cafec6479d9d59e1ad182fdd0b732
yanganyun <237364436@qq.com>
2018-08-16 15:44:12 +0800

    优化

--- components/pageComponents/activity/details/Meau.vue
+++ components/pageComponents/activity/details/Meau.vue
@@ -67,7 +67,7 @@
 				box-shadow: 0px 2px 6px 0px rgba(53, 58, 63, 0.1);
 				position: fixed;
 				top: 0;
-				z-index: 99999;
+				z-index: 999;
 				ul {
 					width: 1170px;
 					position: fixed;

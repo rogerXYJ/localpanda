82da68a28b97ffc3102288a6adef7374ecde55dd
yanganyun <237364436@qq.com>
2018-08-17 14:51:18 +0800

    优化PC支付

--- assets/js/plugin/l10n.js
+++ assets/js/plugin/l10n.js
@@ -4,7 +4,11 @@ const localeIndex = isGithubPages ? 2 : 1;
 window.__exampleLocale = window.location.pathname.split('/')[localeIndex] || 'en';
 const urlPrefix = isGithubPages ? '/elements-examples/' : '/';
 
-document.querySelectorAll('.optionList a').forEach(function(langNode) {
+
+var optionListA = document.querySelectorAll('.optionList a');
+
+for(var i=0;i<optionListA.length;i++){
+  var langNode = optionListA[i];
   const langValue = langNode.getAttribute('data-lang');
   const langUrl = langValue === 'en' ? urlPrefix : (urlPrefix + langValue + '/');
 
@@ -15,4 +19,17 @@ document.querySelectorAll('.optionList a').forEach(function(langNode) {
     langNode.setAttribute('href', langUrl);
     langNode.parentNode.setAttribute('aria-selected', 'false');
   }
-});
+}
+
+// document.querySelectorAll('.optionList a').forEach(function(langNode) {
+//   const langValue = langNode.getAttribute('data-lang');
+//   const langUrl = langValue === 'en' ? urlPrefix : (urlPrefix + langValue + '/');
+
+//   if (langUrl === window.location.pathname || langUrl === window.location.pathname + '/') {
+//     langNode.className += ' selected';
+//     langNode.parentNode.setAttribute('aria-selected', 'true');
+//   } else {
+//     langNode.setAttribute('href', langUrl);
+//     langNode.parentNode.setAttribute('aria-selected', 'false');
+//   }
+// });
